b3310243fd8ea784acc19eb15cc6c9a6991a3888
Nikta-Endeka2004 <endeka04062004@gmail.com>
2024-06-16 15:13:23 +0300

    Add start of front

---
+++ my-electron-app/frontend/src/pages/User.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const User: FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default User
---
+++ my-electron-app/frontend/src/pages/Works.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const Works: FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Works

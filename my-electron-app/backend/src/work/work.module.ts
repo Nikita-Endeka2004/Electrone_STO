482534b0ea8b058ffd6d07e4c4beb66087f77456
Nikta-Endeka2004 <endeka04062004@gmail.com>
2024-06-16 14:47:47 +0300

    Add full backend side

--- my-electron-app/backend/src/work/work.module.ts
+++ my-electron-app/backend/src/work/work.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { WorkService } from './work.service';
 import { WorkController } from './work.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Work } from './entities/work.entity';
+import { User } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Work, User]), UserModule],
   controllers: [WorkController],
-  providers: [WorkService],
+  providers: [WorkService, UserService],
 })
 export class WorkModule {}
--- my-electron-app/backend/src/work/work.service.ts
+++ my-electron-app/backend/src/work/work.service.ts
@@ -1,26 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateWorkDto } from './dto/create-work.dto';
 import { UpdateWorkDto } from './dto/update-work.dto';
+import { UserService } from '../user/user.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Work } from './entities/work.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class WorkService {
-  create(createWorkDto: CreateWorkDto) {
-    return 'This action adds a new work';
+  constructor(@InjectRepository(Work) private readonly workRepository: Repository<Work>,
+  private readonly userService: UserService
+){}
+  async create(createWorkDto: CreateWorkDto) {
+    const latestUser = await this.userService.findLatest()
+    const user = latestUser[0]
+    const newWork = {
+      work: createWorkDto.work,
+      amount: createWorkDto.amount,
+      count: createWorkDto.count,
+      user: {
+        id: user.id
+      }
+    }
+    return await this.workRepository.save(newWork)
   }
 
-  findAll() {
-    return `This action returns all work`;
+  async findAll() {
+    const latestUser = await this.userService.findLatest()
+    const user = latestUser[0]
+    return await this.workRepository.find({
+      where: {
+        user: {id: user.id}
+      }
+    })
   }
 
   findOne(id: number) {
     return `This action returns a #${id} work`;
   }
 
-  update(id: number, updateWorkDto: UpdateWorkDto) {
-    return `This action updates a #${id} work`;
+  async update(id: number, updateWorkDto: UpdateWorkDto) {
+    const work = await this.workRepository.findOne({
+      where: {id}
+    })
+    if(!work) throw new NotFoundException('Work not found!')
+    return await this.workRepository.update(id, updateWorkDto)
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} work`;
+  async remove(id: number) {
+    const work = await this.workRepository.findOne({
+      where: {id}
+    })
+    if(!work) throw new NotFoundException('Work not found!')
+    return await this.workRepository.delete(id)
   }
 }
